20dfca05910a617ebbe18d51732831c29880da76
xdeepakv <deepak.m.shrma@gmail.com>
2018-11-17 02:08:04 +0800

    clean up node modules from copy

--- templates/cli.js
+++ templates/cli.js
@@ -7,15 +7,14 @@ var { src, dest } = require('gulp');
 yargs.command(
   'new [name]',
   'Create new CLI app',
-  () => {},
+  () => { },
   args => {
     if (!args.name) {
       console.log(colors.red('Please provide name for [CLI App]'));
       process.exit(1);
     }
     const cwd = process.cwd();
-    console.log(__dirname + '../**/*')
-    src(__dirname + '/../**/*').pipe(dest(`${cwd}/${args.name}`));
+    src(['**/*', '!node_modules/**/*'], { cwd: __dirname + '/../' }).pipe(dest(`${cwd}/${args.name}`));
   },
 );
 yargs.option('verbose', {
